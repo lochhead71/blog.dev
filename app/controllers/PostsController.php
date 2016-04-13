43f09c0f6ad3657c1cc50ad5fe64553531fdc0c8
Jim Lochhead <lochhead71@gmail.com>
2016-04-13 13:41:42 -0500

    Created a Post model for writing to the database; updated PostsController to navigate between the post resources.

---
+++ app/controllers/PostsController.php
@@ -0,0 +1,103 @@
+<?php
+
+class PostsController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$posts = Post::paginate(4);
+		return View::make('posts.index')->with(['posts' => $posts]);
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return View::make('posts.create');
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{	
+	    // create the validator
+	    $validator = Validator::make(Input::all(), Post::$rules);
+
+	    // attempt validation
+	    if ($validator->fails()) {
+	        // validation failed, redirect to the post create page with validation errors and old inputs
+	        return Redirect::back()->withInput()->withErrors($validator);
+	    } else {
+		$post = new Post();
+		$post->title = Input::get('title');
+		$post->body  = Input::get('body');
+		$post->save();			
+		}
+
+
+
+		return Redirect::action('posts.index');
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$post = Post::find($id);
+		return View::make('posts.show')->with('post', $post);
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		echo 'Will show a form for editing a specific post.';
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		echo 'Will update a specific post.';
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		echo 'Will delete a specific post.';
+	}
+
+
+}
---
+++ app/models/Post.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Post extends Eloquent 
+{
+	public static $rules = [
+	    'title'	=> 'required|max:100|min:3',
+	    'body'	=> 'required|max:10000'
+];
+	protected $table = 'posts';
+}
+
+?>
