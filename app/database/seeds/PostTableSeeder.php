d5621ed2b712beb9a6d62a24bd64b2fe8aeccdc2
Jim Lochhead <lochhead71@gmail.com>
2016-04-15 11:11:45 -0500

    Created a user migration file, foriegn keys for users in the post table, and seeders for both tables

---
+++ app/database/seeds/PostTableSeeder.php
@@ -0,0 +1,29 @@
+<?php 
+
+class PostTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$post = new Post();
+		$post->title = 'Hello World!';
+		$post->body = 'This is my first post utilizing the Larvel framework I\'m using to build this website. I\'ve included it in my seeder.';
+		$post->user_id = User::first()->id;
+		$post->save();
+
+		$post = new Post();
+		$post->title = 'Follow up post';
+		$post->body = 'This is my second post; I\'m excited how user-friendly this development tool, freeing me up to focus on custom features and the look and feel of the user interface.. thanks, Laravel!';
+		$post->user_id = User::first()->id;
+		$post->save();
+
+		$post = new Post();
+		$post->title = 'Third time\'s the charm!';
+		$post->body = 'This is third post in my seeder. My mother used to dabble in interior design when I was growing up and instilled the preference for triptychs... Google it!';
+		$post->user_id = User::first()->id;
+		$post->save();
+
+	}
+
+}
+
+?>
--- app/models/Post.php
+++ app/models/Post.php
@@ -1,12 +1,25 @@
-<?php 
+<?php
 
-class Post extends Eloquent 
+use Carbon\Carbon;
+
+class Post extends BaseModel 
 {
 	public static $rules = [
 	    'title'	=> 'required|max:100|min:3',
 	    'body'	=> 'required|max:10000'
-];
+	];
+
 	protected $table = 'posts';
+
+	public function setBodyAttribute($body)
+	{
+		$this->attributes['body'] = strip_tags($body);
+	}
+
+	public function user()
+	{
+	    return $this->belongsTo('User');
+	}
 }
 
 ?>
